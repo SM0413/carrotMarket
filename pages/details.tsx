16df8b8471edbc80716f09579c860a479424d787
SM0413 <kbjtmdals@naver.com>
2022-09-08 10:24:53 +0900

    More Modifiers

---
+++ pages/details.tsx
@@ -0,0 +1,20 @@
+export default function Detailes() {
+  return (
+    <div className="p-5 flex flex-col space-y-2">
+      <details className="select-none open:text-white open:bg-indigo-500">
+        <summary className="cursor-pointer">What is my fav. food</summary>
+        <span className="">김치</span>
+      </details>
+      <ul className="list-decimal marker:text-teal-500">
+        <li>HI</li>
+        <li>HI</li>
+        <li>HI</li>
+      </ul>
+      <input
+        type="file"
+        className="file:transition-colors file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border-2 file:cursor-pointer file:border-0 file:rounded-xl file:px-5 file:bg-purple-400 file:text-white "
+      />
+      <p className="first-letter:text-7xl">lorem ipsum lalalalalalala</p>
+    </div>
+  );
+}
