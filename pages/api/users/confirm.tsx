f726ee8057e04ddfcd33fb957f9a9515a2497d23
SM0413 <kbjtmdals@naver.com>
2022-09-14 10:37:38 +0900

    Serverless Sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import client from "@libs/server/client";
 import withHandler, { IResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
@@ -7,6 +8,21 @@ async function handler(
   res: NextApiResponse<IResponseType>
 ) {
   const { token } = req.body;
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+    include: { user: true },
+  });
+  if (!exists) return res.status(404).end();
+  req.session.user = {
+    id: exists.userId,
+  };
+  await req.session.save();
+  res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotSession",
+  password: process.env.COOKIE_PW!,
+});
