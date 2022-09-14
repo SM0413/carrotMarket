e2c875ca0110614d84173898c94b293dcdb2c2eb
SM0413 <kbjtmdals@naver.com>
2022-09-14 17:45:06 +0900

    Counting Relationships

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,16 @@ import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 import Head from "next/head";
 import useSWR from "swr";
-import { Product } from "@prisma/client";
+import { Fav, Product } from "@prisma/client";
 
+interface ProductWithCount extends Product {
+  _count: {
+    fav: number;
+  };
+}
 interface IProductResponse {
   ok: boolean;
-  products: Product[];
+  products: ProductWithCount[];
 }
 
 const Home: NextPage = () => {
@@ -20,35 +25,39 @@ const Home: NextPage = () => {
       <Head>
         <title>Home</title>
       </Head>
-      <div className="flex flex-col space-y-5 divide-y">
-        {data?.products?.map((product) => (
-          <Item
-            id={product.id}
-            key={product.id}
-            title={product.name}
-            price={product.price}
-            comments={1}
-            hearts={1}
-          />
-        ))}
-        <FloatingButton href="/products/upload">
-          <svg
-            className="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      {!data ? (
+        "Loading..."
+      ) : (
+        <div className="flex flex-col space-y-5 divide-y">
+          {data?.products?.map((product) => (
+            <Item
+              id={product.id}
+              key={product.id}
+              title={product.name}
+              price={product.price}
+              comments={1}
+              hearts={product._count.fav}
             />
-          </svg>
-        </FloatingButton>
-      </div>
+          ))}
+          <FloatingButton href="/products/upload">
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              />
+            </svg>
+          </FloatingButton>
+        </div>
+      )}
     </Layout>
   );
 };
