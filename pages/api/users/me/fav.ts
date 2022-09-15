6fe790f2a228bb739879dbfe66aa9fb0c2d8be4c
SM0413 <kbjtmdals@naver.com>
2022-09-15 17:09:45 +0900

    Handlers

---
+++ pages/api/users/me/fav.ts
@@ -0,0 +1,24 @@
+import client from "@libs/server/client";
+import withHandler, { IResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+  const favs = await client.fav.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+  res.json({ ok: true, favs });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
