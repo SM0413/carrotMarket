110e7cd3b82f9bafdf381330ec1466d6ce853590
SM0413 <kbjtmdals@naver.com>
2022-09-15 15:42:31 +0900

    useCoords

--- pages/api/posts/index.tsx
+++ pages/api/posts/index.tsx
@@ -1,23 +1,34 @@
-import client from "@libs/server/client";
-import withHandler, { IResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { IResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponseType>
 ) {
   const {
-    body: { question },
+    body: { question, latitude, longitude },
     session: { user },
   } = req;
-
   if (req.method === "POST") {
     const post = await client.post.create({
-      data: { question, user: { connect: { id: user?.id } } },
+      data: {
+        question,
+        latitude,
+        longitude,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
     });
-    res.json({ ok: true, post });
-  } else if (req.method === "GET") {
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
     const posts = await client.post.findMany({
       include: {
         user: {
@@ -35,10 +46,15 @@ async function handler(
         },
       },
     });
-    res.json({ ok: true, posts });
+    res.json({
+      ok: true,
+      posts,
+    });
   }
 }
-
 export default withApiSession(
-  withHandler({ methods: ["GET", "POST"], handler })
+  withHandler({
+    methods: ["GET", "POST"],
+    handler,
+  })
 );
