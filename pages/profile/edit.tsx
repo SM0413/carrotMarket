0e03201df26f56baef0a707a7bae59c34bdc638a
SM0413 <kbjtmdals@naver.com>
2022-09-16 15:06:42 +0900

    Image Preview

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -3,14 +3,16 @@ import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useUser from "@libs/client/useUser";
 import useMutation from "@libs/client/useMutation";
+import useSWR from "swr";
 
 interface IEditProfileForm {
   name?: string;
   email?: string;
   phone?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -25,6 +27,7 @@ const EditProfile: NextPage = () => {
     register,
     setValue,
     handleSubmit,
+    watch,
     setError,
     formState: { errors },
   } = useForm<IEditProfileForm>();
@@ -35,7 +38,7 @@ const EditProfile: NextPage = () => {
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<IEditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name }: IEditProfileForm) => {
+  const onValid = ({ email, phone, name, avatar }: IEditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name === "") {
       return setError("formErrors", {
@@ -53,17 +56,33 @@ const EditProfile: NextPage = () => {
       setError("formErrors", { message: data.error });
     }
   }, [data, setError]);
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const avatar = watch("avatar");
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
   return (
     <Layout canGoBack title="Edit Profile">
       <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <img className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
